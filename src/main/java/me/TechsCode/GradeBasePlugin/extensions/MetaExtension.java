afa5cbb69dbdc5c6628c422b28edb65ae5a76841
Fabian Dingemans <mail@fabiandingemans.nl>
2024-01-23 21:40:25 +0100

    Fixed an issue with isAPI

--- src/main/java/me/TechsCode/GradeBasePlugin/extensions/MetaExtension.java
+++ src/main/java/me/TechsCode/GradeBasePlugin/extensions/MetaExtension.java
@@ -12,7 +12,7 @@ public class MetaExtension {
     public String loadAfter, loadBefore, load;
     public ArrayList<String> libraries;
     public boolean fetch;
-    public boolean isAPI = false;
+    public boolean isAPI;
 
     public boolean configValid() {
         if (pluginVersion == null) {
@@ -26,6 +26,38 @@ public class MetaExtension {
             GradleBasePlugin.log();
             GradleBasePlugin.log(Color.RED + "Please check the build.gradle of your project");
             return false;
+        }
+        if (loadAfter == null) {
+            GradleBasePlugin.log("Could not find a 'loadAfter' field in your build.gradle");
+            GradleBasePlugin.log();
+            GradleBasePlugin.log(Color.RED + "Please check the build.gradle of your project");
+            return false;
+        }
+        if (loadBefore == null) {
+            GradleBasePlugin.log("Could not find a 'loadBefore' field in your build.gradle");
+            GradleBasePlugin.log();
+            GradleBasePlugin.log(Color.RED + "Please check the build.gradle of your project");
+            return false;
+        }
+        if (load == null) {
+            GradleBasePlugin.log("Could not find a 'load' field in your build.gradle");
+            GradleBasePlugin.log();
+            GradleBasePlugin.log(Color.RED + "Please check the build.gradle of your project");
+            return false;
+        }
+        if (libraries == null) {
+            GradleBasePlugin.log("Could not find a 'libraries' field in your build.gradle");
+            GradleBasePlugin.log();
+            GradleBasePlugin.log(Color.RED + "Please check the build.gradle of your project");
+            return false;
+        }
+        if (fetch) {
+            if(System.getenv("TECHSCODE_USERNAME") == null || System.getenv("TECHSCODE_PASSWORD") == null) {
+                GradleBasePlugin.log("Could not find a 'TECHSCODE_USERNAME' or 'TECHSCODE_PASSWORD' environment variable");
+                GradleBasePlugin.log();
+                GradleBasePlugin.log(Color.RED + "Please set the 'TECHSCODE_USERNAME' and 'TECHSCODE_PASSWORD' environment variables");
+                return false;
+            }
         }
 
         return true;
